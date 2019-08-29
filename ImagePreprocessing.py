cbadd8f60d1f4e2a32f1748a7c4d5709357cace1
Krish Moran <moran.kri@husky.neu.edu>
2019-08-29 17:03:46 -0400

    progress on extracting from csv

--- ImagePreprocessing.py
+++ ImagePreprocessing.py
@@ -2,7 +2,20 @@ import numpy as np
 import cv2
 import os
 import time, sys
+import pandas as pd
 
+# Constants
+input_patient_folder = '/Users/krishmoran/Documents/LungClassification/Krish/SamplePatients/Patient_4274'
+key_slice_file = '/Users/krishmoran/Downloads/DL_info.csv'
+
+# Get Key Slice Number
+
+df = pd.read_csv(key_slice_file)
+file_names = df.File_name
+#file_names is a list of strings with the name of the file
+x=file_names[1]
+x[:6]
+key_slices = df.Key_slice_index
 
 # Hidden File Ignore
 # <editor-fold desc="Hidden File Ignore">
@@ -36,7 +49,7 @@ def load_image(fpath):
     # returns the matrix of the image 
     return normalizedImg
 
-# given the folder path and filename (e.g. '000.png'), returns the full filepath 
+# given the folder path and filename (e.g. '000.png'), returns the full filepath (OS X Dir)
 def get_path(folder, filename):
     return folder + '/' + filename
 
@@ -48,32 +61,40 @@ def stack_slices(patient_folder, key_slice):
     slices = list(listdir_nohidden(patient_folder))
     slices.sort()
 
-    # creates a new list of the corresponding slice indices for easier searcing
+    # creates a new list of the corresponding slice indices (ints for easier searcing
     sl_indices = []
     for str in slices:
         sl_indices.append(int(str[:3]))
 
     # determines the first (lowest indexed) slice to be added
-    infslice = key_slice - 2
-    # superior slice = key_slice + 2
+    # slice = 0 (top/superior)
+    # inferior slice = key_slice + 2
+    sup_slice = key_slice - 2
 
     for i in range(len(slices)):
-        if sl_indices[i] == infslice:
+        if sl_indices[i] == sup_slice:
             x = load_image(get_path(patient_folder, slices[i]))
             patient1_slices.append(x)
             print("Slice Extracted:", sl_indices[i])
 
-            # once the inferior slice is found, the subsequent 4 slices are added to the list
+            # once the superior slice is found, the subsequent 4 slices are added to the list
             for j in range(i + 1, i + 5):
                 x = load_image(get_path(patient_folder, slices[j]))
                 print("Slice Extracted:", sl_indices[j])
                 patient1_slices.append(x)
                 
-    # ims = [im.astype(float) for im in patient1_slices]
-    pat = patient1_slices
-    im = np.concatenate(patient1_slices)
-    # final_img = np.stack(patient1_slices)
-    print(im)
+
+    # im = np.concatenate(patient1_slices)
+    final_img = np.stack(patient1_slices)
+    # print(len(patient1_slices))
+    print(final_img.shape)
+    return final_img
+
+
+stack_slices(input_patient_folder,)
+
+    # TODO: add return statement once slices are stacked 
+
 
 stack_slices('/Users/krishmoran/Documents/LungClassification/Krish/SamplePatients/Patient_4274', 126)
 
